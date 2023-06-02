3a9ff564b2d5483eb0f81b653cf89e7683163181
Synth-Axis <synth-etiks@hotmail.com>
2023-06-02 20:44:41 +0100

    Blog Post responsive update

--- warcraft_daily/src/pages/blogpost/PostCard.jsx
+++ warcraft_daily/src/pages/blogpost/PostCard.jsx
@@ -3,16 +3,14 @@ import "./PostCard.scss";
 const PostCard = (props) => {
   return (
     <div className="post-card-container">
-      <div>
-        <div className="post-image">
-          <img src={props.data.image} alt="" />
-        </div>
-        <p className="post-card-author">
-          By <span>{`${props.data.author} |`}</span> {props.data.date}
-        </p>
-        <h3 className="post-card-title">{props.data.title}</h3>
-        <p className="post-card-description">{props.data.description}</p>
+      <div className="post-image">
+        <img src={props.data.image} alt="" />
       </div>
+      <p className="post-card-author">
+        By <span>{`${props.data.author} |`}</span> {props.data.date}
+      </p>
+      <h3 className="post-card-title">{props.data.title}</h3>
+      <p className="post-card-description">{props.data.description}</p>
     </div>
   );
 };
